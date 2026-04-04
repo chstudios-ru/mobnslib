23f170205da24079c646c47ec86a62ea18014e99
velimvim <sliwkimyau@gmail.com>
2026-04-04 19:51:17 +0300

    переписаны запросы в более (надеюсь) читабельный вид

--- MobNsLib.py
+++ MobNsLib.py
@@ -5,34 +5,13 @@ class nsLib:
     def __init__(self, url):
         self.session = requests.Session()
 
-        url1 = 'https://mobile.ir-tech.ru'
-        url2 = 'https://esia.gosuslugi.ru'
-        url3 = 'https://identity.ir-tech.ru'
-        appVer = '1.3.9'
-        lng = "&lng=ru"
-        self.vars = {
-            'client_secret':"04064338-13df-4747-8dea-69849f9ecdf0",
-            
-            'url_1':url + "/webapi/auth/login-state",
-            'url_2':url + "/webapi/sso/esia/crosslogin?loginState=",
-            'url_2_end':"&esia_permissions=1&esia_role=1",
-            'url_3':url + "/webapi/sso/esia/account-info?loginState=",
-            'url_4':url + "/webapi/auth/login",
-            'url_5':url + "/webapi/mysettings/mobile/pincode",
-            'url_6':url + "/api/mobile/users?v=2&appVersion=" + appVer + lng,
-            'url_logout':url + "/logout",
-            'url_diary':url + "/api/mobile/classmeetings?studentIds=",
-            'url_diary_end':"&extraActivity=null&appVersion=" + appVer + lng,
-            'url_init':url + "/api/mobile/initialize?version=" + appVer,
-
-            'url1_ver':f"{url1}/api/v1/mobile/parent/app-versions/published?appVersion={appVer}{lng}",
-
-            'url2_1':url2 + "/rs/dscl?ondate=",
-            'url2_2':url2 + "/aas/oauth2/api/login/",
-
-            'url3_1':url3 + "/connect/token",
-            'url3_2':url3 + "/users/endpoints?appVersion=" + appVer + lng
-        }
+        self.url = url
+        self.url1 = 'https://mobile.ir-tech.ru'
+        self.url2 = 'https://esia.gosuslugi.ru'
+        self.url3 = 'https://identity.ir-tech.ru'
+        self.appVer = '1.3.9'
+        self.lng = "ru"
+        self.client_secret = '04064338-13df-4747-8dea-69849f9ecdf0'
 
     def esiaLogin(self, filename=None, login_esia=None):
 
@@ -47,38 +26,65 @@ class nsLib:
             'MAX':'otp-max'
         }
         data = {'mobile':'1'}
-        response = self.session.post(self.vars['url_1'], data=data)
+        response = self.session.post(
+            f"{self.url}/webapi/auth/login-state",
+            data=data
+        )
         loginState = response.text
         loginState = loginState.replace('"','')
 
-        temp_url = self.vars['url_2'] + loginState + self.vars['url_2_end']
-        response = self.session.get(temp_url)
+        response = self.session.get(
+            f"{self.url}/webapi/sso/esia/crosslogin",
+            params={
+                'loginState':loginState,
+                'esia_permissions':'1',
+                'esia_role':'1'
+            }
+        )
 
         pattern = '%d-%m-%Y_%H-%M-%S'
         ondate = datetime.now().strftime(pattern)
-        self.session.get(self.vars['url2_1']+ondate)
+        self.session.get(
+            f"{self.url2}/rs/dscl",
+            params={
+            'ondate':ondate
+            }
+        )
 
         if login_esia == None:
             login_esia = {
             'login': input("Ваш логин:"),
             'password': getpass.getpass("Ваш пароль:")
             }
-        response = self.session.post(self.vars['url2_2'], json=login_esia)
+        response = self.session.post(
+            f"{self.url2}/aas/oauth2/api/login/", 
+            json=login_esia
+        )
         print(response.text)
         if str(response.status_code) == '201':
             print("Неправильные данные")
+            return None
         else:
             mfa_type_asked = response.json().get('mfa_details').get('type')
             mfa_code = input(f"Введите код {totp[mfa_type_asked]}:")
-            response = self.session.post(self.vars['url2_2'] + url[mfa_type_asked] + '/verify?code=' + mfa_code)
-
+            response = self.session.post(
+                f"{self.url2}/aas/oauth2/api/login/{url[mfa_type_asked]}/verify"
+                params={
+                    'code':mfa_code
+                }
+            )
             if response.json().get("action") == "MAX_QUIZ":
                 print(response.text)
-                response = self.session.post(self.vars['url2_2'] + 'quiz-max/skip')
+                response = self.session.post(
+                    f"{self.url2}/aas/oauth2/api/login/quiz-max/skip"
+                )
             tmp = response.json().get('redirect_url')
             response = self.session.get(tmp)
 
-            response = self.session.get(self.vars['url_3']+loginState)
+            response = self.session.get(
+                f"{self.url}/webapi/sso/esia/account-info", 
+                params={'loginState':loginState}
+            )
             tmp = response.json()
 
             id = tmp['users'][0]['id']
@@ -88,29 +94,40 @@ class nsLib:
                 'LoginType':'8',
                 'lscope':id
             }
-            response = self.session.post(self.vars['url_4'], data)
+            response = self.session.post(
+                f"{self.url}/webapi/auth/login",
+                data=data)
             at = response.json()['at']
 
             headers = {
                 'at':at
             }
-            response = self.session.get(self.vars['url_5'], headers=headers)
+            response = self.session.get(
+                f"{self.url}/webapi/mysettings/mobile/pincode",
+                headers=headers
+            )
             device_code = response.json()['userCode']
 
             data = {
                 'grant_type':'urn:ietf:params:oauth:grant-type:device_code',
                 'device_code':device_code,
                 'client_id':'parent-mobile',
-                'client_secret':self.vars['client_secret']
+                'client_secret':self.client_secret
             }
-            response = self.session.post(self.vars['url3_1'], data=data)
+            response = self.session.post(
+                f"{self.url3}/connect/token",
+                data=data
+            )
             tokens = {
                 'access_token':response.json()['access_token'],
                 'refresh_token':response.json()['refresh_token'],
                 'expires_in':response.json()['expires_in']
             }
 
-            self.session.get(self.vars['url_logout'], headers=headers)
+            self.session.get(
+                f"{self.url}/logout",
+                headers=headers
+            )
             if filename != None:
                 with open(filename, 'w', encoding='utf_8') as n:
                     json.dump(tokens, n)
@@ -118,10 +135,25 @@ class nsLib:
         return {'authorization':f"Bearer {tokens.get('access_token')}"}
 
     def getInfo(self, headers):
-        response = self.session.get(self.vars['url3_2'], headers=headers)
+        response = self.session.get(
+            f"{self.url3}/users/endpoints", 
+            headers=headers,
+            params={
+                'appVersion':self.appVer,
+                'lng':self.lng
+            }
+        )
         serverId = response.json()[0]['serverId']
 
-        response = self.session.get(self.vars['url_6'], headers=headers)
+        response = self.session.get(
+            f"{self.url}/api/mobile/users",
+            headers=headers,
+            params={
+                'v':'2',
+                'appVersion':self.appVer,
+                'lng':self.lng
+            }
+        )
 
         info = {
             'serverId':serverId,
@@ -151,7 +183,18 @@ class nsLib:
         if startDate == None and endDate == None:
             startDate, endDate = self.get_week_range('%Y-%m-%d', day)
 
-        response = self.session.get(f"{self.vars['url_diary']}{studentId}&startDate={startDate}&endDate={endDate}{self.vars['url_diary_end']}", headers=headers)
+        response = self.session.get(
+            f"{self.url}/api/mobile/classmeetings",
+            params={
+                'studentIds':studentId,
+                'startDate':startDate,
+                'endDate':endDate,
+                'extraActivity':'null',
+                'appVersion':self.appVer,
+                'lng':self.lng 
+            },
+            headers=headers
+        )
 
         with open(diaryName, 'w', encoding='utf_8') as diary:
             diary.write(response.text)
@@ -169,9 +212,12 @@ class nsLib:
             'grant_type':'refresh_token',
             'refresh_token':refresh_token,
             'client_id':'parent-mobile',
-            'client_secret':self.vars['client_secret']
+            'client_secret':self.client_secret
         }
-        response = requests.post(self.vars['url3_1'], data=data)
+        response = requests.post(
+            f"{self.url3}/connect/token",
+            data=data
+        )
         tokens = {
                 'access_token':response.json()['access_token'],
                 'refresh_token':response.json()['refresh_token'],
